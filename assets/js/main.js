8ccee4763c911f2b22a02d81b52f0e1b7df56b98
Nick Bowers <greennick7777@gmail.com>
2017-12-28 18:04:37 -0500

    Fixes error in javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,3 @@
-require('smoothscroll-polyfill').polyfill();
-
 const navButton = document.getElementById('nav-btn');
 const navItems = document.getElementsByTagName('nav')[0].getElementsByTagName('a');
 const closedStyle = {transform: 'translateX(340px)'};
