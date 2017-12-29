41820b8e9425af560e7cee48e05717a63e84de96
Nick Bowers <greennick7777@gmail.com>
2017-12-28 21:14:12 -0500

    Rewrites all javascript into jquery for Edge and Safari compatibility

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,33 @@
-const navButton = document.getElementById('nav-btn');
-const navItems = document.getElementsByTagName('nav')[0].getElementsByTagName('a');
 const closedStyle = {transform: 'translateX(340px)'};
 const openStyle = {transform: 'translateX(0px)'};
 let navState = false;
 
-const toggleNav = style => {
-  const nav = document.getElementsByTagName('nav');
-
-  Object.assign(nav[0].style, style);
-  navState = !navState;
-}
-
-navButton.addEventListener('click', event => {
-  (navState === false) ? toggleNav(openStyle) : toggleNav(closedStyle);
+$(document).ready(function() {
+  scrollTo(400);
+  navPress();
 });
 
-const addListenersToNodes = list => {
-  for (let item = 0; item < 11; item++) {
-    list[item].addEventListener('click', event => {
-      let section = list[item].getAttribute('data-section');
-      let target = document.getElementById(section);
+const scrollTo = duration => {
+  $('a[href|="#section"').on('click', function(event) {
+    let target = $($(this).attr('href'));
 
-      window.scroll({top: target.offsetTop, left: 0, behavior: 'smooth'});
-      toggleNav(closedStyle);
-    });
-  }
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: target.offset().top
+    }, duration);
+    toggleNav(closedStyle);
+  });
 }
 
-addListenersToNodes(navItems);
+const navPress = () => {
+  let navButton = $('#nav-btn');
+  navButton.on('click', event => {
+    (navState === false) ? toggleNav(openStyle) : toggleNav(closedStyle);
+  });
+}
+
+const toggleNav = style => {
+  const $nav = $('nav');
+  $nav.css(style);
+  navState = !navState;
+}
