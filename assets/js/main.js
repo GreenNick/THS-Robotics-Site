2518fc5a2a428a38c4d51d8ae8dc1f4a8930e0c6
Nick Bowers <greennick7777@gmail.com>
2017-12-29 22:15:36 -0500

    Refactors javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,23 @@
-const closedStyle = {transform: 'translateX(340px)'};
-const openStyle = {transform: 'translateX(0px)'};
-let navState = false;
-
-$(document).ready(function() {
+$(document).ready(() => {
   scrollTo(400);
   navPress();
 });
 
 const scrollTo = duration => {
-  $('a[href|="#section"').on('click', function(event) {
-    let target = $($(this).attr('href'));
+  $('a[href|="#section"').on('click', event => {
+    let target = $($(event.currentTarget).attr('href'));
 
     event.preventDefault();
     $('html, body').animate({
       scrollTop: target.offset().top
     }, duration);
-    toggleNav(closedStyle);
+    $('nav').toggleClass('nav-active');
   });
 }
 
 const navPress = () => {
-  let navButton = $('#nav-btn');
-  navButton.on('click', event => {
-    (navState === false) ? toggleNav(openStyle) : toggleNav(closedStyle);
+  const $navButton = $('#nav-btn');
+  $navButton.on('click', event => {
+    $('nav').toggleClass('nav-active');
   });
-}
-
-const toggleNav = style => {
-  const $nav = $('nav');
-  $nav.css(style);
-  navState = !navState;
 }
